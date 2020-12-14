d743fbb4efd05c6a952a35e100619aa90f562cd4
maxdexter <maxdexter@bk.ru>
2020-12-14 14:46:58 +0300

    перенес весь функционал из MainActivity.kt в ResultFragment.kt лигику из MainViewModel в ResultViewModel.kt

--- app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/MainActivity.kt
+++ app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/MainActivity.kt
@@ -19,74 +19,12 @@ import ru.maxdexter.translatorcoincoroutine.databinding.ActivityMainBinding
 class MainActivity: AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    private var mainAdapter: MainAdapter? = null
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initFab()
-        renderData()
-        initRecycler()
+
     }
 
-    private fun initRecycler() {
-        mainAdapter = MainAdapter()
-        binding.recycler.apply {
-            adapter = mainAdapter
-            layoutManager = LinearLayoutManager(context)
-        }
-    }
-
-    private fun initFab() {
-        binding.searchFab.setOnClickListener {
-            val fragment = SearchFragment.newInstance()
-            fragment.setClickListener {
-                viewModel.getData(it, true)
-            }
-            supportFragmentManager.beginTransaction().add(fragment, "BOTTOM_SHEET")
-                .commitAllowingStateLoss()
-        }
-    }
-
-    private fun renderData() {
-        viewModel.appState.observe(this, { appState ->
-            when (appState) {
-                is AppState.Success -> {
-                        showViewSuccess()
-                        appState.data?.let { mainAdapter?.setData(it) }
-                        mainAdapter?.setItemClickListener {}
-                }
-                is AppState.Error -> {
-                    showViewError()
-                }
-                is AppState.Loading -> {
-                    showViewLoading()
-                    binding.reloadButton.setOnClickListener {
-                        recreate()
-                    }
-                }
-            }
-        })
-    }
-
-
-    private fun showViewError() {
-        binding.successLinearLayout.visibility = android.view.View.GONE
-        binding.loadingFrameLayout.visibility = android.view.View.GONE
-        binding.errorLinearLayout.visibility = android.view.View.VISIBLE
-    }
-
-
-    private fun showViewSuccess() {
-        binding.successLinearLayout.visibility = android.view.View.VISIBLE
-        binding.loadingFrameLayout.visibility = android.view.View.GONE
-        binding.errorLinearLayout.visibility = android.view.View.GONE
-    }
-
-    private fun showViewLoading() {
-        binding.successLinearLayout.visibility = android.view.View.GONE
-        binding.loadingFrameLayout.visibility = android.view.View.VISIBLE
-        binding.errorLinearLayout.visibility = android.view.View.GONE
-    }
 }
--- app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/resultefragment/ResultFragment.kt
+++ app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/resultefragment/ResultFragment.kt
@@ -5,27 +5,84 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.maxdexter.mytranslatorkoincoroutines.adapter.MainAdapter
+import ru.maxdexter.mytranslatorkoincoroutines.model.AppState
 import ru.maxdexter.translatorcoincoroutine.R
+import ru.maxdexter.translatorcoincoroutine.databinding.ResultFragmentBinding
 
 class ResultFragment : Fragment() {
 
     companion object {
         fun newInstance() = ResultFragment()
     }
-
+    private var mainAdapter: MainAdapter? = null
     private lateinit var viewModel: ResultViewModel
+    private lateinit var binding: ResultFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.result_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.result_fragment, container, false)
+        renderData()
+        initRecycler()
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ResultViewModel::class.java)
+    }
+
+    private fun initRecycler() {
+        mainAdapter = MainAdapter()
+        binding.recycler.apply {
+            adapter = mainAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun renderData() {
+        viewModel.appState.observe(viewLifecycleOwner, { appState ->
+            when (appState) {
+                is AppState.Success -> {
+                    showViewSuccess()
+                    appState.data?.let { mainAdapter?.setData(it) }
+                    mainAdapter?.setItemClickListener {}
+                }
+                is AppState.Error -> {
+                    showViewError()
+                }
+                is AppState.Loading -> {
+                    showViewLoading()
+                    binding.reloadButton.setOnClickListener {
+                    }
+                }
+            }
+        })
+    }
+
+
+    private fun showViewError() {
+        binding.successLinearLayout.visibility = android.view.View.GONE
+        binding.loadingFrameLayout.visibility = android.view.View.GONE
+        binding.errorLinearLayout.visibility = android.view.View.VISIBLE
+    }
+
+
+    private fun showViewSuccess() {
+        binding.successLinearLayout.visibility = android.view.View.VISIBLE
+        binding.loadingFrameLayout.visibility = android.view.View.GONE
+        binding.errorLinearLayout.visibility = android.view.View.GONE
+    }
+
+    private fun showViewLoading() {
+        binding.successLinearLayout.visibility = android.view.View.GONE
+        binding.loadingFrameLayout.visibility = android.view.View.VISIBLE
+        binding.errorLinearLayout.visibility = android.view.View.GONE
     }
 
 }
