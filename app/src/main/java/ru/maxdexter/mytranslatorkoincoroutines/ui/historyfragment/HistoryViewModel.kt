3b4fb11b8a5729ead979eac1485e5150d4a02229
maxdexter <maxdexter@bk.ru>
2020-12-16 11:20:38 +0300

    добавил HistoryFragment.kt и вьюмодель

---
+++ app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/historyfragment/HistoryViewModel.kt
@@ -0,0 +1,7 @@
+package ru.maxdexter.mytranslatorkoincoroutines.ui.historyfragment
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
