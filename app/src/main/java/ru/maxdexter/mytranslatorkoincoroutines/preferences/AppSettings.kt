3bd33a499a9a0b503e4ccfe65afe6b8411819c9f
maxdexter <maxdexter@bk.ru>
2020-12-29 16:50:51 +0300

    добавил темную тему

--- app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/preferences/AppSettings.kt
+++ app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/preferences/AppSettings.kt
@@ -1,9 +1,10 @@
 package ru.maxdexter.mytranslatorkoincoroutines.preferences
 
+import android.app.Activity
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 
-class AppSettings(private val activity: AppCompatActivity) {
+class AppSettings(private val activity: Activity) {
     private val preferences = activity.getPreferences(AppCompatActivity.MODE_PRIVATE)
     var isDarkTheme: Boolean by PreferencesDelegate(preferences, IS_DARK_THEME,false)
 
--- app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/MainActivity.kt
+++ app/src/main/java/ru/maxdexter/mytranslatorkoincoroutines/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.maxdexter.mytranslatorkoincoroutines.ui
 
+import android.app.Application
 import android.content.SharedPreferences
 import android.content.res.Configuration
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -23,8 +26,14 @@ class MainActivity: AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val isDarkTheme = AppSettings(this).isDarkTheme
+            if (isDarkTheme) {
+                setTheme(R.style.AppThemeNight)
+            } else {
+                setTheme(R.style.AppTheme)
+            }
+
         super.onCreate(savedInstanceState)
-        val currentTheme = AppSettings(this).isDarkTheme
         AppSettings(this).isDarkTheme = true
 
 
